0af0a9c98a1603488c7e9f85e96b821cf195d671
Joao Victor Moreira de Figueiredo <figueiredo1194@gmail.com>
2020-07-21 21:44:24 -0300

    Create function remove_math_prefixes

--- clubes/functions.py
+++ clubes/functions.py
@@ -6,3 +6,15 @@ def webpage_requests (url_site):
     soup = BeautifulSoup(response.content, 'lxml')
 
     return soup
+
+
+def remove_math_prefixes(dataframe, column_name):
+    math_prefixes = ['bi','Bi','tri','Tri','tetra','Tetra','penta','Penta',
+                     'hexa','Hexa','hepta','Hepta','octa','Octa','enea','Enea','deca','Deca']
+    
+    for i in math_prefixes:
+        for j in range(dataframe.shape[0]):
+            if i in dataframe[column_name][j]:
+                dataframe.loc[j,column_name] = dataframe.loc[j,column_name].replace(i,'')
+    
+    return dataframe[column_name]
