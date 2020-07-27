372f62d3009ff6476fb274a7016990c3a6c8f54a
Joao Victor Moreira de Figueiredo <figueiredo1194@gmail.com>
2020-07-27 15:40:38 -0300

    Add save_to_csv function

--- clubes/functions.py
+++ clubes/functions.py
@@ -1,12 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+import os
 
 def webpage_requests (url_site):
     response = requests.get(url_site)
     soup = BeautifulSoup(response.content, 'lxml')
 
     return soup
-
 
 def remove_math_prefixes(dataframe, column_name):
     math_prefixes = ['bi','Bi','tri','Tri','tetra','Tetra','penta','Penta',
@@ -18,3 +19,10 @@ def remove_math_prefixes(dataframe, column_name):
                 dataframe.loc[j,column_name] = dataframe.loc[j,column_name].replace(i,'')
     
     return dataframe[column_name]
+
+def save_to_csv(dataframe, file_name):
+    if os.path.exists('files'):
+        dataframe.to_csv('files/'+file_name+'.csv', sep=',', encoding='iso-8859-1', index=False)
+    else:
+        os.mkdir('files')
+        dataframe.to_csv('files/'+file_name+'.csv', sep=',', encoding='iso-8859-1', index=False)
