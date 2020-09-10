eaf83ad8c43cff086848e0b0092f182ce359a93a
Joao Victor Moreira de Figueiredo <figueiredo1194@gmail.com>
2020-09-10 12:03:23 -0300

    Adaptation of the cruzeiro.py file to adjust with new changes in webpage_requests function

--- clubes/cruzeiro.py
+++ clubes/cruzeiro.py
@@ -1,45 +1,57 @@
 import pandas as pd
 import functions
+from bs4 import BeautifulSoup
+import zipfile
+import hashlib
 
 def cruzeiro_gather():
     try:
+        with zipfile.ZipFile('offline_pages/offline_pages.zip','r') as zip_file:
+            zip_file.extractall(path='offline_pages/')
+            with open('offline_pages/cruzeiro_10092020.html', 'rb') as file:
+                page = file.read()
+
+        page_text = BeautifulSoup(page,'lxml').text
+        local_hash = hashlib.md5(page_text.encode('utf-8')).hexdigest()   
+        
         # Url from Cruzeiro conquests page
-        soup = functions.webpage_requests('https://www.cruzeiro.com.br/conquistas')
+        website_content = functions.webpage_requests('https://www.cruzeiro.com.br/conquistas')
 
-        # Create empty lists to get data
-        nome_titulos = []
-        anos_titulos = []
-        anos_outros_titulos = []
+        if local_hash == website_content[0]:
+            # Create empty lists to get data
+            nome_titulos = []
+            anos_titulos = []
+            anos_outros_titulos = []
 
-        # Get the number total of conquests from 'TÍTULOS INTERNACIONAIS' and 'TÍTULOS NACIONAIS'
-        total_titulos = len(soup.find_all('div', class_='card card-person card-conq')) 
+            soup = website_content[1]
 
-        for i in range(total_titulos):
-            # Iterate to get the names of conquests
-            nome_titulos.append(soup.find_all('a', 'card-title valign-wrapper')[i].get_text().strip())
+            # Get the number total of conquests from 'TÍTULOS INTERNACIONAIS' and 'TÍTULOS NACIONAIS'
+            total_titulos = len(soup.find_all('div', class_='card card-person card-conq')) 
 
-            # Iterate to get year of conquests
-            anos_titulos.append(soup.find_all('div', class_='card-content')[i].get_text().replace('\n',''))
+            for i in range(total_titulos):
+                # Iterate to get the names of conquests
+                nome_titulos.append(soup.find_all('a', 'card-title valign-wrapper')[i].get_text().strip())
+
+                # Iterate to get year of conquests
+                anos_titulos.append(soup.find_all('div', class_='card-content')[i].get_text().replace('\n',''))
 
         
-        # Get the category 'OUTROS TÍTULOS'
-        nomes_outros_titulos = soup.find_all('h4')
+            # Get the category 'OUTROS TÍTULOS'
+            nomes_outros_titulos = soup.find_all('h4')
 
-        # Get the years from 'OUTROS TÍTULOS'
-        for i in range(len(nomes_outros_titulos)):
-            anos_outros_titulos.append(soup.find_all('p', class_='flow-text')[i].get_text().replace('\n','').strip())
+            # Get the years from 'OUTROS TÍTULOS'
+            for i in range(len(nomes_outros_titulos)):
+                anos_outros_titulos.append(soup.find_all('p', class_='flow-text')[i].get_text().replace('\n','').strip())
     
-        # Need to repeat the name of conquests according to years of these conquests
-        for i in range(len(nomes_outros_titulos)):
-            for j in range(len(anos_outros_titulos[i].split(', '))):
-                nome_titulos.append(nomes_outros_titulos[i].get_text())
+            # Need to repeat the name of conquests according to years of these conquests
+            for i in range(len(nomes_outros_titulos)):
+                for j in range(len(anos_outros_titulos[i].split(', '))):
+                    nome_titulos.append(nomes_outros_titulos[i].get_text())
 
-        # Store the years from 'OUTROS TÍTULOS' in the years of all conquests list
-        for i in range(len(anos_outros_titulos)):
-            anos_titulos.extend(anos_outros_titulos[i].split(', '))
+            # Store the years from 'OUTROS TÍTULOS' in the years of all conquests list
+            for i in range(len(anos_outros_titulos)):
+                anos_titulos.extend(anos_outros_titulos[i].split(', '))
 
-        if len(nome_titulos) == len(anos_titulos):
-            
             df = pd.DataFrame()
             df['titulo'] = nome_titulos
             df['ano_conquista'] = anos_titulos
